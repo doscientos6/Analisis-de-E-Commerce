ca88ec8170385dc54bf010062edb21039467e4d5
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-16 21:29:44 -0300

    Update 01_create_table.sql

--- 01_query-sql/01_create_table.sql
+++ 01_query-sql/01_create_table.sql
@@ -1,1 +1,17 @@
+--
+CREATE TABLE ecommerce_transactions (
+    venta_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    invoice_no VARCHAR(20) NOT NULL,
+    stock_code VARCHAR(20) NOT NULL,
+    description TEXT,
+    quantity INT NOT NULL,
+    invoice_date VARCHAR(50) NOT NULL,  
+    unit_price NUMERIC(10,2) NOT NULL,
+    customer_id INT,
+    country VARCHAR(100) NOT NULL
+);
 
+
+ALTER TABLE ecommerce_transactions 
+  ALTER COLUMN invoice_date TYPE TIMESTAMP 
+  USING to_timestamp(invoice_date, 'MM/DD/YYYY HH24:MI');

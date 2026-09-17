13ddcf945b8865e222df96910f18c377cdc56abd
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-16 21:57:41 -0300

    Update 02_Data_Validation.sql

--- 01_query-sql/02_Data_Validation.sql
+++ 01_query-sql/02_Data_Validation.sql
@@ -1,1 +1,21 @@
 
+--
+SELECT invoice_no, stock_code, quantity, invoice_date, customer_id, COUNT(*)
+FROM ecommerce_transactions
+GROUP BY invoice_no, stock_code, quantity, invoice_date, customer_id
+HAVING COUNT(*) > 1;
+
+--
+
+SELECT venta_id, invoice_no, stock_code, country
+FROM ecommerce_transactions
+WHERE invoice_no != TRIM(invoice_no)
+   OR stock_code != TRIM(stock_code)
+   OR country != TRIM(country);
+
+--
+
+SELECT venta_id, description
+FROM ecommerce_transactions
+WHERE description ~ '[^\x20-\x7E\xC0-\xFF]';
+

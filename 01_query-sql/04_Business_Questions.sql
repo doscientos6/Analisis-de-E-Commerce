5fcffe773a6fc9df8f6ef783658426b83c4aa4d5
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-18 21:25:43 -0300

    Create 04_Business_Questions.sql

---
+++ 01_query-sql/04_Business_Questions.sql
@@ -0,0 +1,5 @@
+Ventas por país.
+Cantidad de clientes recurrentes
+Promedio de cantidad productos comprados por cliente
+Promedio de ventas por mes del año
+Productos con mayor cantidad de unidades vendidas durante el año

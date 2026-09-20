ab681486cad93dc7080e8a85c1f1c36ab7e43ce9
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-19 22:53:32 -0300

    Update 04_Business_Questions.sql

--- 01_query-sql/04_Business_Questions.sql
+++ 01_query-sql/04_Business_Questions.sql
@@ -1,5 +1,5 @@
-
---A. Ventas por país.
+--4. REALIZAMOS PREGUNTAS DE NEGOCIO Y LAS RESPONDEMOS PARA PODER REALIZAR UN ANÁLISIS DEL DATASET
+--A. VENTAS POR PAÍS
 SELECT
   country AS pais,
   SUM(quantity * unit_price) AS ventas_totales
@@ -8,8 +8,8 @@ WHERE invoice_no NOT LIKE 'C%'
 GROUP BY country
 ORDER BY ventas_totales DESC;
 
---B. Cantidad de clientes recurrentes
-WITH compras_por_cliente AS(
+--B. CANTIDAD DE CLIENTES RECURRENTES.
+WITH compras_por_cliente AS (
 SELECT 
     customer_id,
     COUNT(DISTINCT invoice_no) AS cantidad_facturas
@@ -17,14 +17,45 @@ FROM ecommerce_transactions
 WHERE customer_id IS NOT NULL AND invoice_no NOT LIKE 'C%'
 GROUP BY customer_id)
 
-SELECT 
-  customer_id,
-  cantidad_facturas
+SELECT COUNT(*) AS clientes_recurrentes
 FROM compras_por_cliente
 WHERE cantidad_facturas > 1;
 
---C.Promedio de cantidad productos comprados por cliente
+--C. PROMEDIO DE CANTIDAD DE PRODUCTOS COMPRADOS POR CLIENTE.
+WITH productos_por_cliente AS (
+  SELECT
+    customer_id,
+    SUM(quantity) AS total_unidades
+  FROM ecommerce_transactions
+  WHERE customer_id IS NOT NULL AND invoice_no NOT LIKE 'C%'
+  GROUP BY customer_id)
 
---D. Promedio de ventas por mes del año
-  
---E. Productos con mayor cantidad de unidades vendidas durante el año
+SELECT
+  ROUND(AVG(total_unidades), 2) AS unidades_promedio_por_cliente
+FROM productos_por_cliente;
+
+--D. VENTAS POR MES DEL AÑO.
+WITH ventas_por_mes AS (
+  SELECT 
+     EXTRACT(MONTH FROM invoice_date) AS mes_del_año, 
+    SUM(quantity * unit_price) AS ventas_totales
+  FROM ecommerce_transactions
+  WHERE invoice_no NOT LIKE 'C%'
+  GROUP BY mes_del_año)
+
+SELECT 
+  mes_del_año,
+  ventas_totales
+FROM ventas_por_mes
+ORDER BY mes_del_año;
+
+--E. LOS DIEZ PRODUCTOS MÁS VENDIDOS DURANTE EL AÑO.
+SELECT
+  stock_code,
+  description,
+  SUM(quantity) AS unidades_vendidas
+FROM ecommerce_transactions
+WHERE invoice_no NOT LIKE 'C%'
+GROUP BY stock_code, description
+ORDER BY unidades_vendidas DESC
+LIMIT 10;

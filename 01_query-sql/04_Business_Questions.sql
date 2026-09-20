1060a0b0de891bbe9c37fb9f2ba23d698af490bc
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-19 22:30:27 -0300

    Update 04_Business_Questions.sql

--- 01_query-sql/04_Business_Questions.sql
+++ 01_query-sql/04_Business_Questions.sql
@@ -1,12 +1,27 @@
 
 --A. Ventas por país.
 SELECT
-  SUM(quantity) AS ventas,
-  country AS pais
+  country AS pais,
+  SUM(quantity * unit_price) AS ventas_totales
 FROM ecommerce_transactions
+WHERE invoice_no NOT LIKE 'C%'
 GROUP BY country
+ORDER BY ventas_totales DESC;
 
 --B. Cantidad de clientes recurrentes
+WITH compras_por_cliente AS(
+SELECT 
+    customer_id,
+    COUNT(DISTINCT invoice_no) AS cantidad_facturas
+FROM ecommerce_transactions
+WHERE customer_id IS NOT NULL AND invoice_no NOT LIKE 'C%'
+GROUP BY customer_id)
+
+SELECT 
+  customer_id,
+  cantidad_facturas
+FROM compras_por_cliente
+WHERE cantidad_facturas > 1;
 
 --C.Promedio de cantidad productos comprados por cliente
 

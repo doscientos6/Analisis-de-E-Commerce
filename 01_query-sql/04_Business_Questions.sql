9ad8572eff707a41ede2b7d5c557cca4b123a207
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-09-19 22:19:17 -0300

    Update 04_Business_Questions.sql

--- 01_query-sql/04_Business_Questions.sql
+++ 01_query-sql/04_Business_Questions.sql
@@ -1,5 +1,10 @@
-Ventas por país.
-Cantidad de clientes recurrentes
-Promedio de cantidad productos comprados por cliente
-Promedio de ventas por mes del año
-Productos con mayor cantidad de unidades vendidas durante el año
+
+--A. Ventas por país.
+
+--B. Cantidad de clientes recurrentes
+
+--C.Promedio de cantidad productos comprados por cliente
+
+--D. Promedio de ventas por mes del año
+  
+--E. Productos con mayor cantidad de unidades vendidas durante el año
